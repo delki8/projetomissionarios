20f85b4d29494c8cb740df1b75fca6f3c9be1159
André <andflec@gmail.com>
2024-06-19 17:03:30 -0300

    foi retirado o 'alert' exibido quando as imagens eram carregadas com sucesso.

--- formMissionarios.js
+++ formMissionarios.js
@@ -21,9 +21,7 @@ function validateFileTypeAndDimensions(validWidth, validHeight, inputFileId) {
             if (!(width === validWidth && height === validHeight)) {
                 alert(`A largura e a altura devem ser de ${validWidth}x${validHeight} px. Este arquivo está fora dessas dimensões.`);
                 document.getElementById(inputFileId).value = '';
-            } else {
-		alert("Arquivo carregado com sucesso.");
-	    }
+            }
         };
     };
 }
@@ -35,39 +33,3 @@ document.getElementById("colorPicker").addEventListener("input", function() {
 
 
      
-
-/*
-
-     
-document.getElementById('fileInput').addEventListener('change', function() {
-    const fileUpload = document.getElementById('fileInput');
-    const file = fileUpload.files[0];
-
-    if (file) {
-	const reader = new FileReader();
-
-	// Leitura do conteúdo do arquivo como Data URL.
-	reader.readAsDataURL(file);
-	reader.onload = function (e) {
-	    const image = new Image();
-
-	    // Definir a fonte da imagem para a URL codificada em base64.
-	    image.src = e.target.result;
-
-	    // Validação das dimensões da imagem.
-	    image.onload = function () {
-		const height = this.height;
-		const width = this.width;
-		if (!(height === 800 && width === 800)) {
-		    alert("O arquivo foi carregado com sucesso mas a Altura e a Largura devem de 800px. Este arquivo está fora dessas dimensões.");
-		    return false;
-		    document.getElementById('fileInput').value = ''
-		}
-		// alert("Uploaded image has valid Height and Width.");
-		// return true;
-	    };
-	};
-    }
-});
-
-*/
